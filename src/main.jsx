2038f6f51ccd72c2858e977d2784abbeacb10bd5
Pranshmish <pragya636@gmail.com>
2025-06-20 22:09:29 +0530

    Removed SvG logo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { ChakraProvider } from "@chakra-ui/react"; // ✅ This works in v2.6.1
+import { ChakraProvider } from "@chakra-ui/react"; 
 
 
 
